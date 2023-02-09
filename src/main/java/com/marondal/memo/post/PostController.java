8d45a4ddbe66b8728c2404c6af860400bd6def80
asdf <asdf@gmail.com>
2023-02-09 19:51:57 +0900

    [김인규]  - 로그인 확인 기능  - 로그아웃 기능

---
+++ src/main/java/com/marondal/memo/post/PostController.java
@@ -0,0 +1,15 @@
+package com.marondal.memo.post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostController {
+	
+	
+	@GetMapping("/post/list/view")
+	public String listView() {
+		return "post/list";
+	}
+
+}
