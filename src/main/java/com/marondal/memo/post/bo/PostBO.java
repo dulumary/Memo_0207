0964e95e625fba847c0bf5b12f04d505fd0858d2
asdf <asdf@gmail.com>
2023-02-14 20:01:26 +0900

    [김인규]  - 메모 작성, 메모 리스트, 메모 내용 기능 추가

---
+++ src/main/java/com/marondal/memo/post/bo/PostBO.java
@@ -0,0 +1,29 @@
+package com.marondal.memo.post.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.memo.post.dao.PostDAO;
+import com.marondal.memo.post.model.Post;
+
+@Service
+public class PostBO {
+	
+	@Autowired
+	private PostDAO postDAO;
+	
+	
+	public int addPost(int userId, String title, String content) {
+		return postDAO.insertPost(userId, title, content);
+	}
+	
+	public List<Post> getPostList(int userId) {
+		return postDAO.selectPostList(userId);
+	}
+	
+	public Post getPost(int postId) {
+		return postDAO.selectPost(postId);
+	}
+}
